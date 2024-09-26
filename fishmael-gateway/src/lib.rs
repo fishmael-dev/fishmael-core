84df5b7b0fe0186eb94bcb2360db50d3b95022c9
Chromosomologist <chromosomologist@gmail.com>
2024-09-26 20:54:50 +0200

    refactor!: uuu

--- fishmael-gateway/src/lib.rs
+++ fishmael-gateway/src/lib.rs
@@ -4,81 +4,120 @@ use futures::{
     SinkExt,
 };
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use std::{env, sync::Arc, time::Duration};
-use tokio::{net::TcpStream, sync::Mutex, task::JoinHandle, time};
+use serde::Deserialize;
+use serde_json::Value;
+use std::{borrow::Borrow, env, mem, sync::Arc, time::Duration};
+use tokio::{net::TcpStream, sync::Mutex, task::JoinHandle, time::{self, Instant, Interval, MissedTickBehavior}};
 use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
 
 use fishmael_model::{
     event::{
-        identify::{Identify, IdentifyProperties, ShardId},
-        GatewayEvent,
-        Opcode,
-        Payload,
+        hello::Hello, identify::{Identify, IdentifyProperties, ShardId}, ready::Ready, GatewayEvent, Opcode, Payload
     },
     intents::Intents,
 };
 
 
-pub struct Client {
-    pub api_url: String,
-    pub gateway_url: String,
+const GATEWAY_URL: &str = "wss://gateway.discord.gg";
+
+
+type Connection = WebSocketStream<MaybeTlsStream<TcpStream>>;
+
+
+pub struct Session {
+    id: Box<str>,
+    sequence: u64,
+}
+
+
+#[derive(Deserialize)]
+pub struct Thing {
+    op: Opcode,
+    d: Value,
+    s: Option<u64>,
+    t: Option<String>,
+}
+
+
+impl Session {
+    pub fn new(sequence: u64, session_id: String) -> Self {
+        Self {
+            id: session_id.into_boxed_str(),
+            sequence,
+        }
+    }
+
+    pub const fn id(&self) -> &str {
+        &self.id
+    }
+
+    pub const fn sequence(&self) -> u64 {
+        self.sequence
+    }
+
+    pub(crate) fn set_sequence(&mut self, sequence: u64) -> u64 {
+        mem::replace(&mut self.sequence, sequence)
+    } 
+}
+
+
+pub struct Shard {
     pub token: String,
     pub intents: Intents,
     heartbeat: Arc<Mutex<Option<JoinHandle<()>>>>,
-    heartbeat_interval: Option<u64>,
-    rng: Arc<Mutex<StdRng>>,
+    heartbeat_interval: Option<Interval>,
+    rng: StdRng,
     seq: Arc<Mutex<Option<u64>>>,
-    session_id: Option<String>,
+    session: Option<Session>,
+    shard_id: ShardId,
     resume_gateway_url: Option<String>,
 }
 
 
-impl Client {
+impl Shard {
 
     pub fn new(
-        api_url: String,
-        gateway_url: String,
         token: String,
+        shard_id: ShardId,
         intents: Intents,
     ) -> Self {
-        Client {
-            api_url,
-            gateway_url,
-            token,
-            intents,
+        Self {
             heartbeat: Arc::new(Mutex::new(None)),
             heartbeat_interval: None,
-            rng: Arc::new(Mutex::new(StdRng::from_entropy())),
-            seq: Arc::new(Mutex::new(None)),
-            session_id: None,
+            intents,
             resume_gateway_url: None,
+            rng: StdRng::from_entropy(),
+            session: None,
+            seq: Arc::new(Mutex::new(None)),
+            shard_id,
+            token,
         }
     }
 
-    pub async fn connect(mut self) -> Result<()>{
-        let (ws, _) = connect_async(&self.gateway_url).await?;
+    pub async fn connect(&mut self) -> Result<()>{
+        let (ws, _) = connect_async(GATEWAY_URL).await?;
         let (tx, mut rx) = ws.split();
         let tx = Arc::new(Mutex::new(tx));
 
         // Wait for Hello and start heartbeat...
-        if let Some(Ok(Message::Text(msg))) = rx.next().await {
-            match serde_json::from_str(&msg) {
-                Ok(GatewayEvent {
-                    op: Opcode::Hello,
-                    d: Some(Payload::Hello(hello)),
-                    ..
-                }) => {
-                    self.heartbeat_interval = Some(hello.heartbeat_interval);
-                    self.start_heartbeat(Arc::clone(&tx), true).await?;
-                },
-                _ => bail!("Did not receive Hello event!"),
-            }
-        } else {
-            bail!("Did not receive Hello event!");
-        }
+        // if let Some(Ok(Message::Text(msg))) = rx.next().await {
+        //     match serde_json::from_str(&msg) {
+        //         Ok(GatewayEvent {
+        //             op: Opcode::Hello,
+        //             d: Some(Payload::Hello(hello)),
+        //             ..
+        //         }) => {
+        //             self.heartbeat_interval = Some(hello.heartbeat_interval);
+        //             // self.start_heartbeat(Arc::clone(&tx), true).await?;
+        //         },
+        //         _ => bail!("Did not receive Hello event!"),
+        //     }
+        // } else {
+        //     bail!("Did not receive Hello event!");
+        // }
 
         // Send Identify...
-        Client::send_gateway_event(
+        Self::send_gateway_event(
             Arc::clone(&tx),
             GatewayEvent::new(
                 Opcode::Identify,
@@ -91,8 +130,8 @@ impl Client {
                         device: "fishmael".to_string(),
                         os: env::consts::OS.to_string(),
                     },
-                    shard: Some(ShardId::new(0, 1)),
-                    token: self.token.clone(),
+                    shard: Some(self.shard_id),
+                    token: self.token.as_mut().to_string(),
                 })
             ),
         ).await?;
@@ -110,50 +149,55 @@ impl Client {
         Ok(())
     }
 
-    async fn start_heartbeat(
-        &self,
-        tx: Arc<Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>,
-        wait: bool,
-    ) -> Result<()> {
-        let mut heartbeat = self.heartbeat.lock().await;
-        if heartbeat.is_some() {
-            heartbeat.as_mut().unwrap().abort();
-        }
+    // async fn start_heartbeat(
+    //     &mut self,
+    //     tx: Arc<Mutex<SplitSink<Connection, Message>>>,
+    //     wait: bool,
+    // ) -> Result<()> {
+    //     let mut heartbeat = self.heartbeat.lock().await;
+    //     if heartbeat.is_some() {
+    //         heartbeat.as_mut().unwrap().abort();
+    //     }
 
-        if let Some(heartbeat_interval) = self.heartbeat_interval {
-            let loop_seq = Arc::clone(&self.seq);
-            let initial_delay = self.rng.lock().await.gen_range(0..heartbeat_interval);
+    //     if let Some(heartbeat_interval) = self.heartbeat_interval {
+    //         let initial_delay = self.rng.lock().await.gen_range(0..heartbeat_interval);
     
-            println!("Staring heartbeat with interval {} [ms].", heartbeat_interval);
+    //         println!("Staring heartbeat with interval {} [ms].", heartbeat_interval);
 
-            // All of this runs in the background.
-            *heartbeat = Some(tokio::spawn(async move {
-                if wait {
-                    // Sleep a random time from 0..heartbeat_interval for the first
-                    // heartbeat.
-                    println!("Waiting {} [ms] until initial heartbeat.", initial_delay);
-                    time::sleep(Duration::from_millis(initial_delay)).await;
-                }
+    //         // All of this runs in the background.
+    //         *heartbeat = Some(tokio::spawn(async move {
+    //             if wait {
+    //                 // Sleep a random time from 0..heartbeat_interval for the first
+    //                 // heartbeat.
+    //                 println!("Waiting {} [ms] until initial heartbeat.", initial_delay);
+    //                 time::sleep(Duration::from_millis(initial_delay)).await;
+    //             }
    
-                loop {
-                    match Client::send_gateway_event(
-                        Arc::clone(&tx),
-                        GatewayEvent::new(Opcode::Heartbeat, Payload::OptInt(*loop_seq.lock().await)),
-                    ).await {
-                        Ok(_) => time::sleep(Duration::from_millis(heartbeat_interval)).await,
-                        Err(_) => break,  // TODO: log this
-                    };
-                }
-            }));
-        } else {
-            bail!("Heartbeat interval was not set before starting heartbeat!");
-        }
+    //             loop {
+    //                 let sequence = self.session.as_mut()
+    //                     .unwrap()
+    //                     .sequence()
+    //                     .clone()
+    //                     .to_owned();
 
-        Ok(())
-    }
+    //                 match Self::send_gateway_event(
+    //                     Arc::clone(&tx),
+    //                     GatewayEvent::new(Opcode::Heartbeat, Payload::OptInt(Some(sequence))),
+    //                 ).await {
+    //                     Ok(_) => time::sleep(Duration::from_millis(heartbeat_interval)).await,
+    //                     Err(_) => break,  // TODO: log this
+    //                 };
+    //             }
+    //         }));
+    //     } else {
+    //         bail!("Heartbeat interval was not set before starting heartbeat!");
+    //     }
+
+    //     Ok(())
+    // }
 
     async fn send_gateway_event(
-        tx: Arc<Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>,
+        tx: Arc<Mutex<SplitSink<Connection, Message>>>,
         event: GatewayEvent,
     ) -> Result<()> {
         let json = serde_json::json!(event).to_string();
@@ -171,7 +215,7 @@ impl Client {
 
     async fn process_gateway_event(
         &mut self,
-        tx: Arc<Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>,
+        tx: Arc<Mutex<SplitSink<Connection, Message>>>,
         payload: String,
     ) -> Result<()> {
         println!("RECEIVED: {}", payload);
@@ -179,13 +223,18 @@ impl Client {
         match serde_json::from_str(&payload) {
             Ok(GatewayEvent {op, d, t: _, s}) => {
                 if let Some(s) = s {
-                    *self.seq.lock().await = Some(s);
+                    if let Some(ref mut session) = &mut self.session {
+                        session.set_sequence(s);
+                    };
                 }
 
                 match (op, d) {
                     (Opcode::Dispatch, Some(Payload::Ready(ready))) => {
+                        println!("Ready received.");
                         // TODO: Store resume url, implement resuming.
-                        self.session_id = Some(ready.session_id);
+                        let sequence = s.context("missing sequence in ready")?;
+
+                        self.session = Some(Session::new(sequence, ready.session_id));
                         self.resume_gateway_url = Some(ready.resume_gateway_url);
 
                         let id = &ready.guilds.iter().next().unwrap().id;
@@ -196,13 +245,69 @@ impl Client {
                         println!("Heartbeat ACK received.");
                     },
                     (Opcode::Heartbeat, _) => {
+                        println!("Heartbeat received.");
                         // Immediately restart heartbeat...
-                        self.start_heartbeat(tx, false).await?;
+                        // self.start_heartbeat(tx, false).await?;
                     }
                     _ => println!("Unknown event: {}", payload),
                 }
             },
             Err(err) => println!("Failed to deserialise: {:?}", err)
+        }
+
+        Ok(())
+    }
+
+    fn process(&mut self, event: &str) -> Result<()> {
+        let Thing{op, d: event_data, s: maybe_sequence, t: maybe_type} =
+            serde_json::from_str(&event)
+            .context("failed to extract data from event")?;
+
+        match op {
+            Opcode::Dispatch => {
+                let event_type = maybe_type
+                    .context("failed to get event type")?;
+                let sequence = maybe_sequence
+                    .context("failed to get sequence")?;
+
+                match event_type.as_ref() {
+                    "READY" => {
+                        let event = serde_json::from_value::<Ready>(event_data)
+                            .context("failed to deserialise ready event")?;
+
+                        self.resume_gateway_url = Some(event.resume_gateway_url);
+                        self.session = Some(Session::new(sequence, event.session_id));
+                    },
+                    "RESUMED" => {
+                        // TODO: implement resuming
+                    }
+                    _ => {}
+                }
+
+                if let Some(session) = self.session.as_mut() {
+                    session.set_sequence(sequence);
+                }
+            },
+            Opcode::Heartbeat => {
+                // TODO: send heartbeat
+            },
+            Opcode::ACK => {
+                println!("ACK received.");
+                // TODO: track heartbeat responses to check if connection is still alive.
+            }
+            Opcode::Hello => {
+                let event = serde_json::from_value::<Hello>(event_data)
+                    .context("failed to deserialise hello event")?;
+                let heartbeat_interval = Duration::from_millis(event.heartbeat_interval);
+                let jitter = Duration::from_millis(self.rng.gen_range(0..event.heartbeat_interval));
+
+                let mut interval = time::interval_at(Instant::now() + jitter, heartbeat_interval);
+                interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
+                self.heartbeat_interval = Some(interval);
+
+                // TODO: implement reconnection
+            }
+            _ => todo!()
         }
 
         Ok(())
