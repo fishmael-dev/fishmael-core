61c0ef32b6ffa778868b6970a0fc1e74c0aba80f
Chromosomologist <chromosomologist@gmail.com>
2024-09-26 09:52:58 +0200

    feat: actually use dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use tokio;
 use fishmael::{self, models::intents::Intents};
-use anyhow::Result;
+use anyhow::{Context, Result};
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv().context("Failed to find dotenv")?;
+    let token = std::env::var("TOKEN").context("Failed to load token from .env")?;
+    
     let client = fishmael::Client::new(
         "https://discord.com/api/v10".to_string(),
         "wss://gateway.discord.gg".to_string(),
-        "".to_string(),
-        Intents::from_bits_truncate(0),
+        token,
+        Intents::GUILDS,
     );
 
     client.connect().await?;
