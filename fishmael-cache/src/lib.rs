0bd525879d4106ef9c42ddd6019c53e2f41a6d77
Chromosomologist <chromosomologist@gmail.com>
2024-10-01 14:03:52 +0200

    refactor: funky trait thing
    idk why I did this

--- fishmael-cache/src/lib.rs
+++ fishmael-cache/src/lib.rs
@@ -38,11 +38,25 @@ pub trait Cacheable {
 
     fn add_fields_to_cmd(&self, cmd: &mut Cmd) -> (); 
 
-    async fn store(&self, con: &mut MultiplexedConnection) -> Result<(), RedisError> {
-        let mut cmd = redis::cmd("HSET");
-        cmd.arg(self.get_key());
-        self.add_fields_to_cmd(&mut cmd);
+    async fn store(&self, con: &mut MultiplexedConnection) -> Result<(), RedisError>
+    where
+        Self: Sized
+    {
+        redis::cmd("HSET")
+            .arg(self.get_key())
+            .args_from(self)
+            .exec_async(con)
+            .await
+    }
+}
 
-        cmd.exec_async(con).await
+trait ArgsFrom<T> {
+    fn args_from(&mut self, value: T) -> &Self;
+}
+
+impl<T: Cacheable> ArgsFrom<&T> for Cmd {
+    fn args_from(&mut self, value: &T) -> &Self {
+        value.add_fields_to_cmd(self);
+        self
     }
 }
